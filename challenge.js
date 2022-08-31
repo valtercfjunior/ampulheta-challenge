841fef80c46e0bf9964f226f73d9a9bd6c6df670
Valter Junior <79730793+valtercfjunior@users.noreply.github.com>
2022-08-31 00:09:48 -0300

    last update

--- challenge.js
+++ challenge.js
@@ -1,20 +1,23 @@
+//gera a matriz
 function gerarAmpulheta(n) {
 	var matriz = JSON.parse(
 		JSON.stringify(new Array(n).fill(new Array(n).fill(" ")))
 	);
-
+	//preenche base e topo com '#'
 	for (let lin = 0; lin < n; lin++) {
 		if (lin === 0) {
 			matriz[0] = new Array(n).fill("#");
 			matriz[n - 1] = new Array(n).fill("#");
 		}
-
+		//preenche as laterais com '#'
 		matriz[lin][0] = "#";
 		matriz[lin][n - 1] = "#";
 
+		//preenche limites da ampulheta
 		matriz[lin][lin] = "#";
 		matriz[lin][n - 1 - lin] = "#";
 
+		//preenche conteudo
 		for (let c = lin; c < n - lin; c++) {
 			matriz[lin][c] = "#";
 		}
@@ -22,24 +25,20 @@ function gerarAmpulheta(n) {
 	return matriz;
 }
 
+//imprime no console
 const desenharAmpulhetaConsole = function (matriz) {
-	if (matriz.length < 20) {
-		return console.log(
-			"O tamanho da coluna deve ser superior ou igual a 20!"
-		);
-	} else {
-		matriz.forEach((row) => {
-			console.log(row.join(""));
-		});
-		console.log(`n = ${matriz.length}`);
-		const separador = new Array(matriz.length).fill("-").join("");
-		console.log(separador);
-	}
+	matriz.forEach((row) => {
+		console.log(row.join(""));
+	});
+	console.log(`n = ${matriz.length}`);
+	const separador = new Array(matriz.length).fill("-").join("");
+	console.log(separador);
 };
 
+//animacao areia caindo
 function desenharAnimacao(matriz) {
 	let c = 1;
-	let controleInterval = Math.ceil(matriz.length / 2);
+	let controleInterval = Math.ceil(matriz.length / 2 - 1);
 	var interval = setInterval(() => {
 		let linhaBackup = matriz[c];
 		matriz[c] = matriz[matriz.length - 1 - c];
@@ -48,13 +47,16 @@ function desenharAnimacao(matriz) {
 		c++;
 
 		if (c == controleInterval) {
+			console.log("FIM!");
 			clearInterval(interval);
 		}
 	}, 1500);
 }
 
+//altura
 const alturaAmpulheta = 25;
 
+//verificador altura >= 20
 function validaAltura(alturaAmpulheta) {
 	const alturaMinima = 20;
 	return alturaAmpulheta > alturaMinima;
